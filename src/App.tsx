d633050b5efe1f06a17dcc9148bb151613ca048c
Aneesh Edara <jebikoh@macbook-pro.myfiosgateway.com>
2020-11-27 20:52:17 -0500

    Remove unused imports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Layout, Menu, Affix, Row, Col, Timeline, Card, List} from 'antd';
+import {Layout, Menu, Affix, List} from 'antd';
 import {
   GithubOutlined,
   TwitterOutlined,
